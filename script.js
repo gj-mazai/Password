fec21b0bcfdc52ab90fb5617a27ff2e45d331ee2
gj-mazai <80626681+gj-mazai@users.noreply.github.com>
2023-01-11 22:48:36 +0700

    dqwf2qf

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-function qwe(){
-  let counter = 0;
-  const password = "213546";
-  const stroke = document.getElementsByClassName("input")[0].value;
-    if(stroke === password){
-      document.getElementsByClassName("title")[0].innerHTML = "ДА";
-    }else {
-        for (let i = 0; i<stroke.length; i++){
-          password[i] === stroke[i] && counter++;
-        }
-         document.getElementsByClassName("title")[0].innerHTML = `НЕТ))), но кол-во угаданных позиций равно = ${counter}`;
-    }
-  }
-  
-  const btn = document.getElementsByClassName("btn")[0];
-  btn.onclick = function(){
-        qwe();
-  }
 
-  
+function qwe(){
+    let counter = 0;
+    const password = "731515"
+    const stroke = document.getElementsByClassName("input")[0].value;
+      if(stroke === password && stroke.length === 6 ){
+        document.getElementsByClassName("title")[0].innerHTML = "ДА";
+      }else if (stroke !== password && stroke.length === 6 ) {
+          for (let i = 0; i<stroke.length; i++){
+            password[i] === stroke[i] && counter++;
+          }
+           document.getElementsByClassName("title")[0].innerHTML = `НЕТ))), но кол-во угаданных позиций равно = ${counter};`;
+      } else {
+        document.getElementsByClassName("title")[0].innerHTML = `Неа, введено не верное кол-во символов`;
+      }
+    }
+    
+    const btn = document.getElementsByClassName("btn")[0];
+    btn.onclick = function(){
+          qwe();
+    }
